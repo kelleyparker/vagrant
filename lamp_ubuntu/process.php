64a37a88e6ec77ed5836f220aa74b844c96f39f3
Kelley Parker <kelley.parker07@gmail.com>
2023-10-29 20:23:18 -0500

    Corrected errors

---
+++ lamp_ubuntu/process.php
@@ -0,0 +1,25 @@
+<?php
+$servername = "localhost";
+$username = "cliffgoat";
+$password = "password";
+$dbname = "db1";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $sql = "INSERT INTO users (name) VALUES ('$name')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Record inserted successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+$conn->close();
+?>
