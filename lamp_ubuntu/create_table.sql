8d118b91a1fb0f69c5e77435b690cf9079e20ea5
Kelley Parker <kelley.parker07@gmail.com>
2023-10-29 19:43:40 -0500

    Update create_table.sql

--- lamp_ubuntu/create_table.sql
+++ lamp_ubuntu/create_table.sql
@@ -1,24 +1,4 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST["name"];
-
-    // Establish a connection to MySQL
-    $conn = new mysqli("localhost", "root", "password", "db1");
-
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-
-    // Prepare and execute SQL query
-    $sql = "INSERT INTO users (name) VALUES (\'$name\')";
-
-    if ($conn->query($sql) === TRUE) {
-        echo "Record added successfully";
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-    }
-
-    $conn->close();
-}
-?>
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255)
+);
